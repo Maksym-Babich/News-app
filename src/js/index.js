4dfb01b268465474a6905fb4771322305a111572
Andrii-kyryliuk11 <poteriana@gmail.com>
2023-02-18 23:49:23 +0100

    weather

--- src/js/index.js
+++ src/js/index.js
@@ -1,1 +1,20 @@
 import './filter';
+import API from './weather-api';
+let latitude = "";
+let longitude = "";
+const api = new API();
+navigator.geolocation.getCurrentPosition(onLocationSuccess, onLocationError); async function onLocationSuccess(pos) {
+     this.latitude = pos.coords.latitude;
+    this.longitude = pos.coords.longitude;
+     
+  return api.fetchWidthLocation().then((response) => {
+      console.log("🚀 ~ file: index.js:11 ~ onLocationSuccess ~ response", response)
+      return api.createMarkup(response)
+  })
+    
+}
+function onLocationError() {
+ 
+    
+api.standartFetch().then((response) => api.createMarkup(response));
+}
