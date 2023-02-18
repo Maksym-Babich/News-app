694117525903c00823ca8cf9a5aec1cdbd05c7ad
Maksym-Babich <111182214+Maksym-Babich@users.noreply.github.com>
2023-02-18 18:48:27 +0000

    Deploying to gh-pages from @ Maksym-Babich/News-app@fcb064992c0fad0e5b5b1c36eb79ec7e76c6cf5f 🚀

---
+++ index.3f037c9d.js
@@ -0,0 +1,2 @@
+!async function(){const s=await fetch("https://api.nytimes.com/svc/news/v3/content/section-list.json?api-key=A3GIIfyPWHBvfJdoXANwrFAEAGEQbzXw"),t=(await s.json()).results;console.log(t)}();
+//# sourceMappingURL=index.3f037c9d.js.map
