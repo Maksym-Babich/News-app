6b3efdfb7e31998c5c83ec182f152884aad29fbd
BohdanHubin <111269462+BohdanHubin@users.noreply.github.com>
2023-02-18 20:21:07 +0200

    add fetch function

---
+++ src/js/filter.js
@@ -0,0 +1,14 @@
+const KEY = 'A3GIIfyPWHBvfJdoXANwrFAEAGEQbzXw'; 
+
+async function getCategoryList() {
+  const fetchApi = await fetch(`https://api.nytimes.com/svc/news/v3/content/section-list.json?api-key=${KEY}`);
+  const response = await fetchApi.json();
+  const  categoriesAray  = response.results;
+
+//лишаю console.log щоб можна було подивитись що приходить в результаті роботи функції
+ console.log(categoriesAray);    
+
+return categoriesAray;
+}
+// лишаю виклик фунції щоб було видно як вона працює
+getCategoryList()
