b9afe02a9d088a1809aaa5d8b3212bd95953a1f3
Faibysh <faybysh.b@gmail.com>
2023-02-19 15:21:04 +0100

    create button

--- src/js/filter.js
+++ src/js/filter.js
@@ -1,29 +1,91 @@
-const KEY = 'A3GIIfyPWHBvfJdoXANwrFAEAGEQbzXw'; 
+const KEY = 'A3GIIfyPWHBvfJdoXANwrFAEAGEQbzXw';
 
 export default async function getCategoryList() {
-  const fetchApi = await fetch(`https://api.nytimes.com/svc/news/v3/content/section-list.json?api-key=${KEY}`);
+  const fetchApi = await fetch(
+    `https://api.nytimes.com/svc/news/v3/content/section-list.json?api-key=${KEY}`
+  );
   const response = await fetchApi.json();
-  const  categoriesAray  = response.results;
-  
-    return categoriesAray;
+  const categoriesAray = response.results;
+  return categoriesAray;
 }
-
-renderCategoryList()
 
 async function renderCategoryList() {
-    let buttonsQuantity = 0;
-    // мобілка
-    if (window.innerWidth < 768) {
-        getCategoryList().then(categoryList => { renderMarkupFilter(categoryList, buttonsQuantity)})
-    }
-    // планшет
-     if (window.innerWidth >= 768 && window.innerWidth < 1280) {
-         buttonsQuantity = 4;
-        getCategoryList().then(categoryList => { renderMarkupFilter(categoryList, buttonsQuantity) })
-    }
-    // десктоп 
-    if (window.innerWidth >= 1280) {
-        buttonsQuantity = 6;
-        getCategoryList().then(categoryList => { renderMarkupFilter(categoryList, buttonsQuantity)})
-    }
+  let buttonsQuantity = 0;
+  if (window.innerWidth < 768) {
+    getCategoryList().then(categoryList => {
+      renderMarkupFilter(categoryList, buttonsQuantity);
+    });
+  }
+  if (window.innerWidth >= 768 && window.innerWidth < 1280) {
+    buttonsQuantity = 4;
+    getCategoryList().then(categoryList => {
+      renderMarkupFilter(categoryList, buttonsQuantity);
+    });
+  }
+  if (window.innerWidth >= 1280) {
+    buttonsQuantity = 6;
+    getCategoryList().then(categoryList => {
+      renderMarkupFilter(categoryList, buttonsQuantity);
+    });
+  }
 }
+
+const renderMarkupFilter = (array, amount) => {
+  const buttonsArray = array.slice(0, amount);
+  const dropdownArray = array.slice(amount, -1);
+  let readyMarkup = [];
+  readyMarkup = buttonsArray.map(
+    button =>
+      `<button data-section=${button.section} class="btn">${button.display_name}</button>`
+  );
+  readyMarkup.push(`
+    <div class="btn show-more">
+  <span class="show-more_btn">Others</span>
+  
+  <div class="show-more__categories">
+  ${dropdownArray
+    .map(
+      item =>
+        `<button class="show-more__category" data-section=${item.section}>${item.display_name}</button>`
+    )
+    .join('')}
+  </div>
+</div>
+  `);
+
+  document
+    .getElementById('buttons-container')
+    .insertAdjacentHTML('beforeend', readyMarkup.join(''));
+  document
+    .querySelector('.show-more')
+    .addEventListener('click', () =>
+      document
+        .querySelector('.show-more__categories')
+        .classList.toggle('show-more__categories--open')
+    );
+  onClickSection();
+};
+
+function onClickSection() {
+  const buttons = document.querySelectorAll('.btn');
+  let activeButton = null;
+  buttons.forEach(button => {
+    button.addEventListener('click', e => {
+      if (activeButton !== null) {
+        activeButton.classList.remove('active');
+      }
+      button.classList.add('active');
+      activeButton = button;
+      buttons.forEach(btn => {
+        if (btn === activeButton) {
+          btn.style.backgroundColor = '#4440F6';
+          btn.style.color = '#fff';
+        } else {
+          btn.style.backgroundColor = '';
+          btn.style.color = '#4440F6 ';
+        }
+      });
+    });
+  });
+}
+renderCategoryList();
