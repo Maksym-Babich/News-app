91bc7e123dd525902108662eb9fe6882b05f9c0e
Maksym-Babich <maxus1042@gmail.com>
2023-02-21 01:30:15 +0200

    +

--- src/js/read.js
+++ src/js/read.js
@@ -1,1 +1,14 @@
 import './header';
+
+const readByDateList = document.querySelector('.read-by-date-list');
+
+const onHideButtonClick = function (event) {
+  const hideBtn = event.target.closest('.hide-button');
+  if (!hideBtn) {
+    return;
+  }
+  const readByDateContainer = hideBtn.closest('.read-by-date-container');
+  readByDateContainer.classList.toggle('list-hidden');
+};
+
+readByDateList.addEventListener('click', onHideButtonClick);
