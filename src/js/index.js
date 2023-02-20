b5d3af761cf72def8375a06b558692409740b713
Andrii-kyryliuk11 <poteriana@gmail.com>
2023-02-20 23:54:47 +0100

    solveProblem

--- src/js/index.js
+++ src/js/index.js
@@ -1,20 +1,12 @@
-import './header';
-
 import './filter';
-import './markup';
 
 
 import WEATHER_API from './weather-api';
 import NEWS_API from './news-api';
 
 const weatherApi = new WEATHER_API();
-
 const newsApi = new NEWS_API();
 let query = document.querySelector('input')
-
-const newsApi = new NEwS_API();
-let query = document.querySelector('input');
-
 
 newsApi.fetchNewsByQuerry(query).then(response => {
   newsApi.createMarkup(response);
@@ -23,21 +15,16 @@ newsApi.fetchNewsByQuerry(query).then(response => {
 navigator.geolocation.getCurrentPosition(onLocationSuccess, onLocationError);
 
 async function onLocationSuccess(pos) {
-  weatherApi.latitude = pos.coords.latitude;
-  weatherApi.longitude = pos.coords.longitude;
+  this.latitude = pos.coords.latitude;
+  this.longitude = pos.coords.longitude;
 
   return weatherApi.fetchWidthLocation().then(response => {
     weatherApi.createWeatherMarkup(response);
   });
 }
 function onLocationError() {
-
   weatherApi.standartFetch().then(response => weatherApi.createWeatherMarkup(response));
-
-  weatherApi
-    .standartFetch()
-    .then(response => weatherApi.createMarkup(response));
-
 }
 
 import './news-text-hiding';
+
