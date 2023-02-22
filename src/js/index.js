39eae47184fb4914e0445068ed41890bcee946ee
Andrii-kyryliuk11 <poteriana@gmail.com>
2023-02-22 11:58:32 +0100

    1

--- src/js/index.js
+++ src/js/index.js
@@ -4,15 +4,15 @@ import './filter';
 import './markup';
 import './render-news-and-weather';
 
-import WEATHER_API from './weather-api';
+
 import NEWS_API from './news-api';
 import renderNewsAndWeather from './render-news-and-weather';
 import { popularNewsMarkup } from './markup';
 
-const weatherApi = new WEATHER_API();
+
 const newsApi = new NEWS_API();
 
-navigator.geolocation.getCurrentPosition(onLocationSuccess, onLocationError);
+
 
 async function createPopularNews() {
   try {
@@ -25,19 +25,7 @@ async function createPopularNews() {
 
 document.addEventListener('DOMContentLoaded', createPopularNews());
 
-async function onLocationSuccess(pos) {
-  weatherApi.latitude = pos.coords.latitude;
-  weatherApi.longitude = pos.coords.longitude;
 
-  return weatherApi.fetchWidthLocation().then(response => {
-    weatherApi.createWeatherMarkup(response);
-  });
-}
-function onLocationError() {
-  weatherApi
-    .standartFetch()
-    .then(response => weatherApi.createWeatherMarkup(response));
-}
 
 import './news-text-hiding';
 import './favourite';
--- src/js/render-news-and-weather.js
+++ src/js/render-news-and-weather.js
@@ -1,20 +1,120 @@
 import { pageNothingFound } from './markup';
 import WEATHER_API from './weather-api';
-
+import { format } from 'date-fns';
+const API_KEY = '05383c6978b3bc81d3b473e0eed83dd7';
 const cardsContainer = document.querySelector('.news-card__list');
-const weatherApi = new WEATHER_API();
+
 
 let newsAndWeatherMarkupArray = [];
 
-const weatherMarkup = weatherApi.createWeatherMarkup();
+let latitude = '';
+let longitude = '';
 
-export default function renderNewsAndWeather(markupsArr) {
-  if (!markupsArr.length) {
+
+async function noLocation() {
+    const fetchWeather = await fetch(
+    `https://api.openweathermap.org/data/2.5/weather?q=berlin&units=metric&appid=${API_KEY}`
+  );
+    const weatherResponse = await fetchWeather.json();
+  const weatherMarkup = `
+   <li class="weather-info weather news-card__item " >
+   <div class="weather-information">
+       
+       <span class="weather-information__degrees">${Math.floor(
+         weatherResponse.main.temp
+       )}°</span>
+        <div class="weather-information__group">
+            <span class="weather-information__description">
+         ${weatherResponse.weather[0].main}
+            </span>
+            <span class="weather-information__location"><svg>
+    <use href="../images/sprite.svg#location"></use>
+</svg> ${weatherResponse.name} </span>
+        </div>
+        </div>
+        
+      <img src="https://openweathermap.org/img/wn/${
+        weatherResponse.weather[0].icon
+      }@4x.png" alt="${
+    weatherResponse.weather[0].description
+  }" class="weather-img" />
+      
+      <span class="weather-date">${format(new Date(), 'eee')}</span>
+      <span class="weather-date">${format(new Date(), 'dd LLL y')}</span>
+      <a href="https://www.accuweather.com/en" class="weather-link" target="_blank" rel="noreferrer noopener">weather for week</a></li>`;
+return weatherMarkup;
+  }
+
+
+async function widthLocation(pos) {
+ 
+  latitude = pos.coords.latitude;
+  longitude = pos.coords.longitude;
+
+  const fetchWeather = await fetch(
+    `https://api.openweathermap.org/data/2.5/weather?lat=${latitude}&lon=${longitude}&units=metric&appid=${API_KEY}`
+  );
+  const weatherResponse = await fetchWeather.json();
+
+  const weatherMarkup = `
+   <li class="weather-info weather news-card__item " >
+   <div class="weather-information">
+       
+       <span class="weather-information__degrees">${Math.floor(
+         weatherResponse.main.temp
+       )}°</span>
+        <div class="weather-information__group">
+            <span class="weather-information__description">
+         ${weatherResponse.weather[0].main}
+            </span>
+            <span class="weather-information__location"><svg>
+    <use href="../images/sprite.svg#location"></use>
+</svg> ${weatherResponse.name} </span>
+        </div>
+        </div>
+        
+      <img src="https://openweathermap.org/img/wn/${
+        weatherResponse.weather[0].icon
+      }@4x.png" alt="${
+    weatherResponse.weather[0].description
+  }" class="weather-img" />
+      
+      <span class="weather-date">${format(new Date(), 'eee')}</span>
+      <span class="weather-date">${format(new Date(), 'dd LLL y')}</span>
+      <a href="https://www.accuweather.com/en" class="weather-link" target="_blank" rel="noreferrer noopener">weather for week</a></li>`;
+  
+if (window.matchMedia('(max-width: 767px  )').matches) {
+    newsAndWeatherMarkupArray.splice(0, 1, weatherMarkup);
+  }
+  if (
+    window.matchMedia('(min-width: 768px)').matches &&
+    window.matchMedia('(max-width: 1279px)').matches
+  ) {
+    newsAndWeatherMarkupArray.splice(1, 1, weatherMarkup);
+  }
+  if (window.matchMedia('(min-width: 1280px)').matches) {
+    newsAndWeatherMarkupArray.splice(2, 1, weatherMarkup);
+  }
+  
+ 
+  
+cardsContainer.innerHTML = newsAndWeatherMarkupArray.join('');
+}
+
+   navigator.geolocation.getCurrentPosition(widthLocation, noLocation);
+ 
+
+export default async function renderNewsAndWeather(markupsArr) {
+   if (!markupsArr.length) {
     cardsContainer.innerHTML = '';
     pageNothingFound();
   }
 
+  
+   
+    
   newsAndWeatherMarkupArray.push(...markupsArr);
+   const weatherMarkup = await noLocation()
 
   if (window.matchMedia('(max-width: 767px  )').matches) {
     newsAndWeatherMarkupArray.splice(0, 0, weatherMarkup);
@@ -28,6 +128,9 @@ export default function renderNewsAndWeather(markupsArr) {
   if (window.matchMedia('(min-width: 1280px)').matches) {
     newsAndWeatherMarkupArray.splice(2, 0, weatherMarkup);
   }
-  cardsContainer.innerHTML = '';
+cardsContainer.innerHTML = '';
   cardsContainer.innerHTML = newsAndWeatherMarkupArray.join('');
-}
+ return newsAndWeatherMarkupArray
+
+ }
+    
