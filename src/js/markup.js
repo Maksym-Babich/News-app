86a0176b864a3c91b402bae9b003c3614e8c8cae
tetianasvidrak <svidrak.tetiana@gmail.com>
2023-02-23 19:42:37 +0100

    added heart icons in news card markup and added checking for favorites before creating markup

--- src/js/markup.js
+++ src/js/markup.js
@@ -1,6 +1,20 @@
 import { format } from 'date-fns';
 import { textHiding } from './news-text-hiding';
 
+const heartSvg = `<svg class="news-card__btn-icon xmlns="http://www.w3.org/2000/svg" width="16" height="14" viewBox="0 0 16 14" fill="none">
+<path d="M4.66683 1C2.82616 1 1.3335 2.47733 1.3335 4.3C1.3335 5.77133 1.91683 9.26333 7.65883 12.7933C7.76168 12.8559 7.87976 12.889 8.00016 12.889C8.12056 12.889 8.23864 12.8559 8.3415 12.7933C14.0835 9.26333 14.6668 5.77133 14.6668 4.3C14.6668 2.47733 13.1742 1 11.3335 1C9.49283 1 8.00016 3 8.00016 3C8.00016 3 6.5075 1 4.66683 1Z" stroke="#4440F7" stroke-linecap="round" stroke-linejoin="round"/>
+</svg>`;
+const activeHeartSvg = `<svg xmlns="http://www.w3.org/2000/svg" width="14" height="12" viewBox="0 0 14 12" fill="none">
+<path d="M3.66536 0C1.8247 0 0.332031 1.47733 0.332031 3.3C0.332031 4.77133 0.915365 8.26333 6.65736 11.7933C6.76022 11.8559 6.8783 11.889 6.9987 11.889C7.1191 11.889 7.23718 11.8559 7.34003 11.7933C13.082 8.26333 13.6654 4.77133 13.6654 3.3C13.6654 1.47733 12.1727 0 10.332 0C8.49136 0 6.9987 2 6.9987 2C6.9987 2 5.50603 0 3.66536 0Z" fill="#4B48DA"/>
+</svg>`;
+
+const checkFavoriteStorage = url => {
+  let storageState = localStorage.getItem('favorite-news');
+  if (storageState) {
+    return JSON.parse(storageState).find(item => item.url === url);
+  }
+};
+
 const popularNewsMarkup = newsArr => {
   const markup = newsArr.map(newsItem => {
     const { url, id, published_date, section, title, abstract, media } =
@@ -15,11 +29,15 @@ const popularNewsMarkup = newsArr => {
         <div class="news-card__img-wrapper">
           <img class="news-card__img" src=${urlMedia} alt="news image" />
           <span class="news-card__category">${section}</span>
-          <div class="news-card__btn-favorite">
-            <span>Add to favorite</span>
-            <svg class="news-card__btn-icon">
-                <use href="./images/sprite.svg#heart"></use>
-             </svg>
+          <div class="news-card__btn-favorite ${
+            checkFavoriteStorage(url) ? 'remove-from-fav' : ''
+          }">
+            <span>${
+              checkFavoriteStorage(url)
+                ? 'Remove from favorite'
+                : 'Add to favorite'
+            }</span>
+            ${checkFavoriteStorage(url) ? activeHeartSvg : heartSvg}
             </div>
         </div>
         <div class="news-card__text">
@@ -53,11 +71,15 @@ const categoryNewsMarkup = newsArr => {
         <div class="news-card__img-wrapper">
           <img class="news-card__img" src=${urlMedia} alt="news image" />
           <span class="news-card__category">${section}</span>
-          <div class="news-card__btn-favorite">
-            <span>Add to favorite</span>
-            <svg class="news-card__btn-icon">
-                <use href="./images/sprite.svg#heart"></use>
-             </svg>
+          <div class="news-card__btn-favorite ${
+            checkFavoriteStorage(url) ? 'remove-from-fav' : ''
+          }">
+            <span>${
+              checkFavoriteStorage(url)
+                ? 'Remove from favorite'
+                : 'Add to favorite'
+            }</span>
+            ${checkFavoriteStorage(url) ? activeHeartSvg : heartSvg}
             </div>
         </div>
         <div class="news-card__text">
@@ -99,11 +121,15 @@ const searchedNewsMarkup = newsArr => {
         <div class="news-card__img-wrapper">
           <img class="news-card__img" src=${urlMedia} alt="news image" />
           <span class="news-card__category">${section_name}</span>
-          <div class="news-card__btn-favorite">
-            <span>Add to favorite</span>
-            <svg class="news-card__btn-icon">
-                <use href="./images/sprite.svg#heart"></use>
-             </svg>
+          <div class="news-card__btn-favorite  ${
+            checkFavoriteStorage(web_url) ? 'remove-from-fav' : ''
+          }">
+            <span>${
+              checkFavoriteStorage(web_url)
+                ? 'Remove from favorite'
+                : 'Add to favorite'
+            }</span>
+            ${checkFavoriteStorage(web_url) ? activeHeartSvg : heartSvg}
             </div>
         </div>
         <div class="news-card__text">
@@ -124,10 +150,10 @@ const searchedNewsMarkup = newsArr => {
   return markup;
 };
 function pageNothingFound() {
-  const nothing = document.querySelector(".nothing");
+  const nothing = document.querySelector('.nothing');
   const card = document.querySelector('.news-card');
   // card.innerHTML = '';
-  nothing.classList = "none"
+  nothing.classList = 'none';
 }
 
 export {
@@ -135,4 +161,6 @@ export {
   popularNewsMarkup,
   categoryNewsMarkup,
   pageNothingFound,
+  heartSvg,
+  activeHeartSvg,
 };
