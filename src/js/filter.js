dee9804564a779763052ac29edf53bd67394d082
IlyaGubinn <ilya.gubin29@gmail.com>
2023-02-24 14:09:58 +0300

    добавляет рабочий пагинатор для категорий

--- src/js/filter.js
+++ src/js/filter.js
@@ -2,6 +2,43 @@ import { categoryNewsMarkup } from './markup';
 import renderNewsAndWeather from './render-news-and-weather';
 const KEY = 'A3GIIfyPWHBvfJdoXANwrFAEAGEQbzXw';
 
+
+
+
+const pagin = document.querySelector('.pagination--container--categories');
+const paginPop = document.querySelector('.pagination--container');
+const pg = document.getElementById('pagination-categories');
+const btnNextPg = document.querySelector('button.next-page-cat');
+const btnPrevPg = document.querySelector('button.prev-page-cat');
+
+
+const KEY_CATEGORIES ='categories';
+
+import { save, load } from './favourite'
+
+////////////////////////////
+let totalPages = 0;
+ 
+
+let valuePageCat = {
+  curPage: 1,
+  numLinksTwoSide: 1,
+  totalPages: 4,
+  offset: 0,
+  perPage: 0,
+  nextPage: 8,
+  
+};
+
+
+const refs = {
+  pagination: document.querySelector('.pagin-categories'),
+};
+
+
+
+
+////////////////////////////////////////
 export default async function getCategoryList() {
   const fetchApi = await fetch(
     `https://api.nytimes.com/svc/news/v3/content/section-list.json?api-key=${KEY}`
@@ -266,17 +303,17 @@ prevNextIcon.forEach(icon => {
 localStorage.removeItem('VALUE');
 localStorage.removeItem('date');
 
-async function getNewsByCategory(category) {
+async function getNewsByCategory(category,perPage,nextPage) {
   try {
     const searchCategory = encodeURIComponent(category);
 
     const fetchApiByCategory = await fetch(
-      `https://api.nytimes.com/svc/news/v3/content/all/${searchCategory}.json?api-key=${KEY}`
+      `https://api.nytimes.com/svc/news/v3/content/all/${searchCategory}.json?api-key=${KEY}&limit=30`
     );
     const response = await fetchApiByCategory.json();
     const newsByCategory = response.results;
-    console.log(newsByCategory);
-    renderNewsAndWeather(categoryNewsMarkup(newsByCategory));
+    let totalPages = newsByCategory.length / 8;
+    renderNewsAndWeather(categoryNewsMarkup(newsByCategory.slice(perPage, nextPage)));
   } catch (error) {
     console.log(error);
   }
@@ -287,7 +324,212 @@ function onBtnFilterClick() {
   btnsFilter.forEach(btnFilter => {
     btnFilter.addEventListener('click', event => {
       const category = event.target.dataset.section;
-      getNewsByCategory(category);
+
+      if (pagin.classList.contains('pagination_categories-hidden')) {
+        pagin.classList.toggle('pagination_categories-hidden')
+        paginPop.classList.add('pagination-hidden')
+      };
+
+     
+
+      
+      save(KEY_CATEGORIES, category);
+      getNewsByCategory(category,valuePageCat.perPage,valuePageCat.nextPage);
+     
     });
   });
 }
+
+
+
+
+
+
+// /////////////////////////////////////////////////
+
+
+
+
+
+
+
+pagination();
+
+pg.addEventListener('click', e =>  {
+  const ele = e.target;
+
+  if (ele.dataset.page) {
+    const pageNumber = parseInt(e.target.dataset.page, 10);
+
+    valuePageCat.curPage = pageNumber;
+    pagination(valuePageCat);
+     
+    handleButtonLeft();
+    handleButtonRight();
+  if (e.target.dataset.page == '1') {
+
+      
+      valuePageCat.offset = 0;
+      valuePageCat.perPage = 0;
+      valuePageCat.nextPage = 8;
+     
+      
+    } else {
+      let currentPage = Number(e.target.dataset.page);
+      let renderPage = currentPage -= 1;
+      valuePageCat.offset = renderPage * 9;
+      valuePageCat.perPage = renderPage * 8;
+      valuePageCat.nextPage = valuePageCat.perPage + 8;
+
+    }
+    
+  }
+ 
+}
+
+);
+
+
+
+
+  
+
+
+
+
+
+// DYNAMIC PAGINATION
+function pagination() {
+  const { totalPages, curPage, numLinksTwoSide: delta } = valuePageCat;
+
+  const range = delta + 4; // use for handle visible number of links left side
+
+  let render = '';
+  let renderTwoSide = '';
+  let dot = `<li class="pg-item-cat"><a class="pg-link-cat">...</a></li>`;
+  let countTruncate = 0; // use for ellipsis - truncate left side or right side
+
+  // use for truncate two side
+  const numberTruncateLeft = curPage - delta;
+  const numberTruncateRight = curPage + delta;
+
+  let active = '';
+  for (let pos = 1; pos <= totalPages; pos++) {
+    active = pos === curPage ? 'active' : '';
+
+    // truncate
+    if (totalPages >= 2 * range - 1) {
+      if (numberTruncateLeft > 3 && numberTruncateRight < totalPages - 3 + 1) {
+        // truncate 2 side
+        if (pos >= numberTruncateLeft && pos <= numberTruncateRight) {
+          renderTwoSide += renderPage(pos, active);
+        }
+      } else {
+        // truncate left side or right side
+        if (
+          (curPage < range && pos <= range) ||
+          (curPage > totalPages - range && pos >= totalPages - range + 1) ||
+          pos === totalPages ||
+          pos === 1
+        ) {
+          render += renderPage(pos, active);
+        } else {
+          countTruncate++;
+          if (countTruncate === 1) render += dot;
+        }
+      }
+    } else {
+      // not truncate
+      render += renderPage(pos, active);
+    }
+  }
+
+  if (renderTwoSide) {
+    renderTwoSide =
+      renderPage(1) + dot + renderTwoSide + dot + renderPage(totalPages);
+    pg.innerHTML = renderTwoSide;
+  } else {
+    pg.innerHTML = render;
+  }
+}
+
+function renderPage(index, active = '') {
+  return ` <li class="pg-item-cat ${active}" data-page="${index}">
+        <a class="pg-link-cat" href="#">${index}</a>
+    </li>`;
+}
+
+document
+  .querySelector('.pagination--container--categories')
+  .addEventListener('click', function (e) {
+    
+    handleButton(e.target);
+   
+  });
+
+function handleButton(element) {
+  if (element.classList.contains('prev-page-cat')) {
+    valuePageCat.curPage--;
+    handleButtonLeft();
+    btnNextPg.disabled = false;
+  
+  } else if (element.classList.contains('next-page-cat')) {
+    valuePageCat.curPage++;
+    handleButtonRight();
+    btnPrevPg.disabled = false;
+   
+  }
+  pagination();
+}
+function handleButtonLeft() {
+  
+  if (valuePageCat.curPage === 1) {
+    
+    btnPrevPg.disabled = true;
+   
+  } else {
+    btnPrevPg.disabled = false;
+   
+  }
+}
+function handleButtonRight() {
+  
+  if (valuePageCat.curPage === valuePageCat.totalPages) {
+   
+    btnNextPg.disabled = true;
+    
+  } else {
+    btnNextPg.disabled = false;
+   
+  }
+}
+refs.pagination.addEventListener('click', e => {
+  
+  const ele = e.target;
+
+  if (
+    (e.target.classList.contains('next-page-cat') || e.target.classList.contains('btn-next-cat'))&&
+    (valuePageCat.curPage  !== valuePageCat.totalPages)
+     
+  ) {
+     valuePageCat.curPage += 1;
+     valuePageCat.offset += 8;
+     valuePageCat.perPage += 8;
+    valuePageCat.nextPage += 8;
+    window.scrollTo(0, 0);
+     
+  }
+  if (
+    (e.target.classList.contains('prev-page-cat') || e.target.classList.contains('btn-prev-cat')) && 
+    (valuePageCat.curPage !== 1)
+  ) {
+    valuePageCat.curPage -= 1
+    valuePageCat.offset -= 8;
+    valuePageCat.perPage -= 8;
+    valuePageCat.nextPage -= 8;
+    window.scrollTo(0, 0);
+  }
+
+  getNewsByCategory(load(KEY_CATEGORIES),valuePageCat.perPage,valuePageCat.nextPage)
+  console.log(valuePageCat)
+})
