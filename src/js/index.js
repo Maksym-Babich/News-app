379df9eeded5968ad3bff13d43bb07ff8bf29be4
IlyaGubinn <ilya.gubin29@gmail.com>
2023-02-24 14:10:25 +0300

    баг фикс индекса

--- src/js/index.js
+++ src/js/index.js
@@ -1,16 +1,19 @@
 import './header';
-import './pagination/pagination'
+
 import './filter';
 import './markup';
 import './render-news-and-weather';
 import './add-to-read';
-
+import './pagination/pagination';
+// import './pagination/paginator-cat';
+// import './pagination/paginator-serch';
 
 
 import NEWS_API from './news-api';
 import renderNewsAndWeather from './render-news-and-weather';
 import { popularNewsMarkup } from './markup';
 import { fetchRead } from './fetch-read';
+const paginPop = document.querySelector('.pagination--container')
 
 
 const newsApi = new NEWS_API();
@@ -20,7 +23,11 @@ async function createPopularNews(perPage, nextPage) {
   try {
     
     const data = await newsApi.popularNews();
-    renderNewsAndWeather(popularNewsMarkup(data.slice(perPage,nextPage)));
+    if (paginPop.classList.contains('pagination-hidden')) {
+      paginPop.classList.toggle('pagination-hidden')};
+          
+    renderNewsAndWeather(popularNewsMarkup(data.slice(perPage, nextPage)));
+    
     
   } catch (error) {
     console.error(error);
